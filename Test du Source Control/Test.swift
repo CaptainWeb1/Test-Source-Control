21a124e7b78dd970e3fe12a8884ea95e12316114
captain web <contact@captain-web.fr>
2019-05-22 20:36:50 +0200

    Ajout classe test

---
+++ Test du Source Control/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  Test du Source Control
+//
+//  Created by florian kindt on 22/05/2019.
+//  Copyright © 2019 captain web. All rights reserved.
+//
+
+import Foundation
+
+class Test {
+    func salut(){
+        print("Salut")
+    }
+
+}
